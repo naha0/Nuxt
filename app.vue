cc1f59fd8b0362ec8e761fe06023327a4009a5e3
naha0 <780400335@qq.com>
2023-03-25 12:00:19 +0800

    init: nuxt Init

---
+++ app.vue
@@ -0,0 +1,14 @@
+<!--
+ * @Author: naha0 780400335@qq.com
+ * @Date: 2023-03-23 17:33:21
+ * @LastEditors: naha0 780400335@qq.com
+ * @LastEditTime: 2023-03-23 20:13:57
+ * @FilePath: \Nuxt\nuxt-demo\app.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+  <div>
+    <!-- 出口文件 -->
+    <NuxtPage />
+  </div>
+</template>
